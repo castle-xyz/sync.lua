6d36ed1f327fac0d9d43f0f9f15fd86edc4e65da
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-10-31 00:51:35 -0700

    4 clients in test

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
 local entity = require 'entity'
 
+local W, H = 0.5 * love.graphics.getWidth(), 0.5 * love.graphics.getHeight()
 
 
 
 local Player = entity.registerType('Player')
 
 function Player:didSpawn(props)
-    print('player spawned at (' .. props.x .. ', ' .. props.y .. ')')
-    self.x = props.x
-    self.y = props.y
+    self.x = W * math.random()
+    self.y = H * math.random()
 end
 
 function Player:draw()
@@ -21,20 +21,22 @@ end
 
 
 
+-- 4 clients
 
-local server, client
-
+local server
+local clients = {}
 
 function love.update(dt)
     if server then
         server:process()
 
---        for id, ent in pairs(server.owned) do
---            ent:update(dt)
---            server:sync(ent)
---        end
+        for id, ent in pairs(server.owned) do
+            ent:update(dt)
+            server:sync(ent)
+        end
     end
-    if client then
+
+    for _, client in pairs(clients) do
         client:process()
     end
 end
@@ -44,25 +46,34 @@ function love.keypressed(k)
         server = entity.newServer { address = '*:22122' }
     end
     if k == 'c' then
-        client = entity.newClient { address = '192.168.1.80:22122' }
+        for i = 1, 4 do
+            clients[i] = entity.newClient { address = '10.0.1.39:22122' }
+        end
     end
 
-    if k == 'p' then
-        client:spawn('Player', {
-            x = math.random() * love.graphics.getWidth(),
-            y = math.random() * love.graphics.getHeight(),
-        })
+    if k == 'i' then
+        clients[1]:spawn('Player', { })
+    end
+    if k == 'o' then
+        clients[2]:spawn('Player', { })
     end
 end
 
 function love.draw()
-    if client then
+    for i, client in ipairs(clients) do
+        love.graphics.push('all')
+
+        local dx = (require('bit')).band(i - 1, 1) * W
+        local dy = (require('bit')).band(i - 1, 2) / 2 * H
+
+        love.graphics.translate(dx, dy)
+        love.graphics.setScissor(dx, dy, W, H)
         love.graphics.print('client ' .. client.serverPeer:state(), 20, 20)
 
         for id, ent in pairs(client.all) do
             ent:draw()
         end
-    else
-        love.graphics.print('no client', 20, 20)
+
+        love.graphics.pop()
     end
 end
