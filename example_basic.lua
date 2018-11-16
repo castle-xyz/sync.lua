b1b2c9e2d03dbbd3d3828c97ff1f6ecc327f2ec4
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-11-15 17:57:01 -0800

    remove `CASTLE_SERVER` related code, adding that in a different way

--- example_basic.lua
+++ example_basic.lua
@@ -68,13 +68,6 @@ local function keyEvent(key)
     end
 end
 
-function love.load()
-    if CASTLE_SERVER then
-        print("SERVER")
-        server = sync.newServer { address = '*:22122', controllerTypeName = 'Controller' }
-    end
-end
-
 function love.keypressed(key)
     if key == '1' then
         server = sync.newServer { address = '*:22122', controllerTypeName = 'Controller' }
